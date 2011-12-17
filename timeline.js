217ae4fc4c2ce64580ded9afd971e3661f2def68
Tim Hudson <tim.hudson15@gmail.com>
2011-12-16 16:29:27 -0800

    Initial pages commit

---
+++ timeline.js
@@ -0,0 +1,66 @@
+//    Timeline.js
+//    (c) 2010-2011 Tim Hudson
+//    Timeline.js may be freely distributed under the MIT license.
+
+(function(context) {
+  
+  var cache = [];
+
+  var timeline = {};
+
+  // Add animation object which requires start, stop and callback { start: 0, stop: 0.4, callback: function() {} }
+  timeline.add = function(animation) {
+
+    // Cache of all animations
+    cache.push(animation);
+
+    return this;
+  };
+
+  timeline.remove = function(animation) {
+
+    var index = cache.indexOf(animation);
+    
+    if (index !== -1) {
+      cache.splice(index, 1);
+    }
+
+    return this;
+  };
+
+  timeline.run = function(time) {
+
+    function animate() {      
+      var l = cache.length;
+
+      for(var i = 0; i < l; i++) {
+        var animation = cache[i];
+
+        if (time > animation.start && time < animation.stop) {
+          animation.callback(relativePosition(animation.start, animation.stop));
+        }
+        else if (time >= animation.stop) {
+          animation.callback(1);
+        }
+        else {
+          animation.callback(0);
+        }
+      }
+    }
+
+    function relativePosition(start, stop) {
+      var length = stop - start,
+        position = time - start;
+      
+      return position / length;
+    }
+
+    animate();
+
+    return this;
+
+  };
+
+  context.timeline = timeline;
+
+}(this));
